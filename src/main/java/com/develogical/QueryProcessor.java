a2df3e3e40b9533ef14a9a3823ba0b9d93b335b7
ChrisTimperley <christimperley@googlemail.com>
2022-09-19 19:10:32 -0400

    i am oneill cruz

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,13 +3,17 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
-        if (query.toLowerCase().contains("shakespeare")) {
+        String queryLowerCase = query.toLowerCase();
+        if (queryLowerCase.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        if (query.toLowerCase().contains("oneill cruz")) {
+        if (queryLowerCase.contains("oneill cruz")) {
             return ".220 batting average";
+        }
+        if (queryLowerCase.contains("what is your name")) {
+            return "Your name is Oneill Cruz";
         }
         return "";
     }
