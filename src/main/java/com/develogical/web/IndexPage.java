ec48a18a84f32e555520c9315b2f7cc2912844b4
ChrisTimperley <christimperley@googlemail.com>
2022-09-19 18:52:07 -0400

    updated welcome title

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -7,12 +7,12 @@ public class IndexPage extends HtmlPage {
     @Override
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
-                "<h1>Welcome!</h1>" +
+                "<h1>Welcome to Pittsburgh!</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
                   "</form>" +
                 "</p>");
     }
-    
+
 }
